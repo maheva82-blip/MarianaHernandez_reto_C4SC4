bfe1bfe36643be5909df3dc190ddc6f924c9c556
maheva82-blip <maheva82@gmail.com>
2026-07-06 02:58:04 +0000

    streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,48 @@
+# ##############################################
+#  Reto: Conociendo el desempeño de los colaboradores 
+#        el Área de Marketing de Socialize your knowledge
+#    Mariana Hernández
+################################################
+
+import streamlit as st
+import pandas as pd
+
+
+#  1. Código para desplegar el títuloy descripción
+'''
+# Conociendo el desempeño de los colaboradores de
+'''
+
+#  2. Código para desplegar el logotipo de la empresa
+
+
+#  3. Código para incluir un control para seleccionar
+#     el género del empleado
+
+
+#  4. Código para incluir un control para seleccionar el rango del
+#     puntaje de desempeño del empleado
+
+
+#  5. Código para incluir un control para seleccionar
+#     el estado civil del empleado
+
+
+#  6. Código para visualizar la distribución de los
+#     puntajes de desempeño de los empleados
+
+
+#  7. Código para visualizar el promedio de las
+#     horas trabajadas por el género del empleado
+
+
+#  8. Código para visualizar la edad de los empleados
+#     con respecto al salario del empleado
+
+
+#  9. Código para visualizar la relación del promedio
+#  de horas trabajadas versus el puntaje de desempeño
+
+
+#  10. Código para desplegar las conclusiones del
+#      análisis en la aplicación web
